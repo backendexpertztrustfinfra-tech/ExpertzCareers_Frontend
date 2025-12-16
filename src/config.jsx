b44ef179c6bc0a82c84185056c36fb0ac97944f4
Neeraj Chaurasiya <neerajchaurasiya7860@gmail.com>
2025-12-16 16:35:47 +0530

    changing config file

--- src/config.jsx
+++ src/config.jsx
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://expertzcareers-backend.onrender.com";
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://expertzcareers-backend.onrender.com";
+// export const BASE_URL = "http://localhost:3000";
 
 
 
